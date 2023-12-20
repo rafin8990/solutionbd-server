50a8ad7874fa20e7b2be41ab552aaa06de9cdb23
rafin8990 <rafin8990@gmail.com>
2023-12-20 11:37:56 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import httpStatus from 'http-status';
+
+import routes from './app/routes';
+import cookieParser from 'cookie-parser';
+const app: Application = express();
+
+app.use(cors());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1', routes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Solution BD SERVER applicationh running succesfully ');
+});
+
+app.use(globalErrorHandler);
+
+//handle not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
